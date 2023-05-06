9672926284af7bf8d7c7ee68efb53a33b6371f66
Crush0 <54226076+Crush0@users.noreply.github.com>
2023-05-06 21:12:46 +0800

    fixed args fetch_image and fetch_video not work

--- main.py
+++ main.py
@@ -17,7 +17,11 @@ if __name__ == '__main__':
     parser.add_argument('--user_id', type=str, required=True, help='地址栏中用户的ID')
     parser.add_argument('--cookie', type=str, required=True, help="你的Cookie")
     parser.add_argument('--output', type=str, default=os.path.join(dir_name, 'output'), help="输出目录")
-    parser.add_argument('--fetch_image', type=bool, default=True, help="是否抓取图片")
-    parser.add_argument('--fetch_video', type=bool, default=True, help="是否抓取视频")
+    parser.add_argument('--fetch_image', type=str, default=True, help="是否抓取图片")
+    parser.add_argument('--fetch_video', type=str, default=True, help="是否抓取视频")
     args = parser.parse_args()
+    if args.fetch_image in ['False', 'false', 'n', 'no', '0']:
+        args.fetch_image = False
+    if args.fetch_video in ['False', 'false', 'n', 'no', '0']:
+        args.fetch_video = False
     run(args)
